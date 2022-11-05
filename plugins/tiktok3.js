ab06accbd54215b61844818b3e1b51a5b68bea49
MangDTech <mangtech24@gmail.com>
2022-11-05 13:44:36 +0700

    Create tiktok3.js

---
+++ plugins/tiktok3.js
@@ -0,0 +1,17 @@
+import { savefrom } from '@bochilteam/scraper'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    const { author: { nickname }, video, description } = await savefrom(args[0])
+    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
+    if (!url) throw 'Can\'t download video!'
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `
+*Nickname:* ${nickname}
+*Description:* ${description}
+`.trim(), m)
+}
+handler.help = ['tiktok3'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(tiktok3|tt3)$/i
+handler.limit = true
+export default handler
