19c651365782fec8362c9454f733c6de60cdc18e
MangDTech <mangtech24@gmail.com>
2022-11-08 05:18:00 +0700

    Add files via upload

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,15 +1,25 @@
-import hxz from 'hxz-api'
-let handler = async(m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `*Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
-if (!args[0].match(/tiktok/gi)) throw `*Link salah! Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
-let p = await  hxz.ttdownloader(args[0])
-const { nowm, wm, audio } = p
-// made by aine
- conn.sendFile(m.chat, nowm, 'tiktok.mp4', `*${global.wm}*`, m)
+import fs from 'fs'
+let fetch = (await import('node-fetch')).default
+let axios = (await import('axios')).default;
+let handler = async (m, { conn, args, usedPrefix, command, text }) => {
+    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    /*let kos = (await axios.get(API('https://malesin.xyz', '/tiktok2', { url: args[0] } ))).data;
+    if (kos.status != 200) throw kos.message;
+    if (!kos) throw kos.message;*/
+    let res = (await axios.get(API('https://malesin.xyz', '/tiktok', { url: args[0] } ))).data;
+    if (res.status != 200) throw res.message;
+    if (!res) throw res.message;
+    try {
+    let name = conn.getName(m.sender)
+    let fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': `${name}`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./thumbnail.jpg'), thumbnail: fs.readFileSync('./thumbnail.jpg'),sendEphemeral: true}}}
+   await conn.sendButton(m.chat, `
+*Username:* ${res.author}
+*Judul:* ${res.title}`, wm, await (await fetch(res.video)).buffer(), [[`Audio`, usedPrefix + `ta ${text}`]], fkontak)
+} catch { m.reply('terjadi kesalahan') }
 }
+    
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok|tiktokdl)$/i
-handler.limit = true
-handler.group = true
+handler.command = /^(tiktok)$/i
+
 export default handler
