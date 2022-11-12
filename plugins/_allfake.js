6ad7a7d2ef2542ffee227156d6fc18fe7501667d
MangDTech <mangtech24@gmail.com>
2022-11-12 20:23:06 +0700

    Update _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -53,9 +53,9 @@ handler.all = async function (m) {
 		}
 		global.fakeig = {
          contextInfo: { externalAdReply: { showAdAttribution: true,
-            mediaUrl: "https://Instagram.com/bot_whangsaf",
+            mediaUrl: "https://Instagram.com",
             mediaType: "VIDEO",
-            description: "https://Instagram.com/bot_whangsaf", 
+            description: "https://Instagram.com", 
             title: 'Simple Bot Esm',
             body: wm,
             thumbnailUrl: pp,
