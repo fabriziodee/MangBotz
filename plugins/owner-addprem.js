2c768c122c2a2929cce9492ac3056841411867b3
MangDTech <mangtech24@gmail.com>
2022-12-12 14:26:45 +0700

    Update owner-addprem.js

--- plugins/owner-addprem.js
+++ plugins/owner-addprem.js
@@ -1,4 +1,5 @@
-let { MessageType } = require('@adiwajshing/baileys')
+
+const { MessageType } = await import('@adiwajshing/baileys')
 let handler = async (m, { conn, text, usedPrefix }) => {
   function no(number){
     return number.replace(/\s/g,'').replace(/([@+-])/g,'')
@@ -9,7 +10,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
   hl[0] = no(hl[0]) + "@s.whatsapp.net"
   hl[1] = text.split('|')[1]
   
-  if (!text) return conn.reply(m.chat, `*❏ GET NUMBER*\n\n• ${usedPrefix}prem number|days\n*Example:* ${usedPrefix}prem 6289654360447|99\n\n• ${usedPrefix}prem @tag|days\n*Example:* ${usedPrefix}prem @6289654360447|99`, m)
+  if (!text) return conn.reply(m.chat, `*❏ GET NUMBER*\n\n• ${usedPrefix}prem number|days\n*Example:* ${usedPrefix}prem 6281361281833|99\n\n• ${usedPrefix}prem @tag|days\n*Example:* ${usedPrefix}prem @6281361281833|99`, m)
   if (typeof db.data.users[hl[0]] == 'undefined') throw 'Pengguna tidak ada didalam data base'
   var jumlahHari = 86400000 * hl[1]
   // var jumlahHari = 1000 * text
@@ -23,11 +24,10 @@ let handler = async (m, { conn, text, usedPrefix }) => {
 }
 handler.help = ['prem *@tag|days*']
 handler.tags = ['owner']
-handler.command = /^(prem)$/i
+handler.command = /^(addprem|prem)$/i
 handler.owner = true
 handler.fail = null
-module.exports = handler
-
+export default handler
 function msToDate(ms) {
   temp = ms
   days = Math.floor(ms / (24*60*60*1000));
