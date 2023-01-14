7849df8dacc4780909851370e0a850579e2646ec
MangDTech <mangtech24@gmail.com>
2023-01-14 18:18:04 +0700

    Update owner-addprem.js

--- plugins/owner-addprem.js
+++ plugins/owner-addprem.js
@@ -1,42 +1,23 @@
-
-const { MessageType } = await import('@adiwajshing/baileys')
-let handler = async (m, { conn, text, usedPrefix }) => {
-  function no(number){
-    return number.replace(/\s/g,'').replace(/([@+-])/g,'')
-  }
-
-  var hl = []
-  hl[0] = text.split('|')[0]
-  hl[0] = no(hl[0]) + "@s.whatsapp.net"
-  hl[1] = text.split('|')[1]
-  
-  if (!text) return conn.reply(m.chat, `*❏ GET NUMBER*\n\n• ${usedPrefix}prem number|days\n*Example:* ${usedPrefix}prem 6281361281833|99\n\n• ${usedPrefix}prem @tag|days\n*Example:* ${usedPrefix}prem @6281361281833|99`, m)
-  if (typeof db.data.users[hl[0]] == 'undefined') throw 'Pengguna tidak ada didalam data base'
-  var jumlahHari = 86400000 * hl[1]
-  // var jumlahHari = 1000 * text
-  var now = new Date() * 1
-  global.db.data.users[hl[0]].premium = true
-  if (now < global.db.data.users[hl[0]].premiumTime) global.db.data.users[hl[0]].premiumTime += jumlahHari
-  else global.db.data.users[hl[0]].premiumTime = now + jumlahHari
-  conn.reply(m.chat,`*❏ UPGRADE PREMIUM*\n\nBerhasil menambahkan akses premium kepada *@${hl[0].split('@')[0]}* selama *${hl[1]} hari*.\n\n*Premium : ${msToDate(global.db.data.users[hl[0]].premiumTime - now)}*`,m,{ contextInfo: { mentionedJid: [hl[0]] } })
-  conn.reply(hl[0],`*❏ UPGRADE PREMIUM*\n\nBerhasil menambahkan akses premium kepada *@${hl[0].split('@')[0]}* selama *${hl[1]} hari*.\n\n*Premium : ${msToDate(global.db.data.users[hl[0]].premiumTime - now)}*`,m,{ contextInfo: { mentionedJid: [hl[0]] } }) 
-
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    else who = m.chat
+    let user = db.data.users[who]
+    if (!who) throw `tag atau balas pesan yang mau dijadikan premium!`
+    let txt = text.replace('@' + who.split`@`[0], '').trim()
+    if (!txt) throw `mau berapa hari?`
+    if (isNaN(txt)) return m.reply(`hanya nomor mamaskuh!\n\ncontoh:\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
+    var jumlahHari = 86400000 * txt
+    var now = new Date() * 1
+    if (now < user.premiumTime) user.premiumTime += jumlahHari
+    else user.premiumTime = now + jumlahHari
+    user.premium = true
+    m.reply(`Berhasil!\n*${user.name}* sekarang sudah premium ${txt} hari.`)
 }
-handler.help = ['prem *@tag|days*']
+handler.help = ['addprem [@user] <hari>']
 handler.tags = ['owner']
-handler.command = /^(addprem|prem)$/i
-handler.owner = true
-handler.fail = null
+handler.command = /^(add|tambah|\+)p(rem)?$/i
+
+handler.rowner = true
+
 export default handler
-function msToDate(ms) {
-  temp = ms
-  days = Math.floor(ms / (24*60*60*1000));
-  daysms = ms % (24*60*60*1000);
-  hours = Math.floor((daysms)/(60*60*1000));
-  hoursms = ms % (60*60*1000);
-  minutes = Math.floor((hoursms)/(60*1000));
-  minutesms = ms % (60*1000);
-  sec = Math.floor((minutesms)/(1000));
-  return days+"H "+hours+"J "+ minutes + "M";
-  // +minutes+":"+sec;
-}
